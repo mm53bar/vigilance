b7b7a8a48a51b30b74416ff4ccc3b41a7eb37453
Drew Strojny <dstrojny@jestro.com>
2008-09-18 21:15:01 -0600

    added a "your comment is awaiting moderation" message

--- comments.php
+++ comments.php
@@ -25,9 +25,12 @@
 			<div class="c-grav"><?php echo get_avatar( get_comment_author_email(), '80' )?></div>
 			<div class="c-body">
       <div class="c-date"><span><?php comment_date('Y'); ?></span> <?php comment_date('F j'); ?></div>
-				<div class="c-head"><?php comment_author_link() ?></div>
+        <div class="c-head"><?php comment_author_link() ?></div>
+        <?php if ($comment->comment_approved == '0') : ?>
+        <p><em><strong>Please Note:</strong> Your comment is awaiting moderation.</em></p>
+        <?php endif; ?>
 				<?php comment_text() ?>
-         <?php comment_type((''),('Trackback'),('Pingback')); ?>
+        <?php comment_type((''),('Trackback'),('Pingback')); ?>
 				<?php edit_comment_link('edit','<p>','</p>'); ?>
 			</div>
 		</div>
