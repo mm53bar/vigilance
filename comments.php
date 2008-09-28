9fd27795b7b99e46cebf57e55e00d47f2b562d4c
Drew Strojny <dstrojny@jestro.com>
2008-09-28 17:21:48 -0600

    spacing on permalinks was off

--- comments.php
+++ comments.php
@@ -28,7 +28,7 @@
           <span><?php comment_date('Y'); ?></span> <?php comment_date('F j'); ?>
         </div>
         <div class="c-head">
-          <?php comment_author_link() ?><span class="c-permalink"><a href="<?php echo get_permalink(); ?>#comment-<?php comment_ID(); ?>"> permalink</a></span>
+          <?php comment_author_link() ?> <span class="c-permalink"><a href="<?php echo get_permalink(); ?>#comment-<?php comment_ID(); ?>">permalink</a></span>
         </div>
         <?php if ($comment->comment_approved == '0') : ?>
           <p><em><strong>Please Note:</strong> Your comment is awaiting moderation.</em></p>
