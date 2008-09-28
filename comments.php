57a994cebe5089edca6e3789eb51cb8946344a9d
Drew Strojny <dstrojny@jestro.com>
2008-09-28 17:14:29 -0600

    added permalink to comments

--- comments.php
+++ comments.php
@@ -25,7 +25,12 @@
 			<div class="c-grav"><?php echo get_avatar( get_comment_author_email(), '80' )?></div>
 			<div class="c-body">
       <div class="c-date"><span><?php comment_date('Y'); ?></span> <?php comment_date('F j'); ?></div>
-        <div class="c-head"><?php comment_author_link() ?></div>
+        <div class="c-head">
+          <?php comment_author_link() ?>
+          <span class="c-permalink">
+            <a href="<?php echo get_permalink(); ?>#comment-<?php comment_ID(); ?>"> permalink</a>
+          </span>
+        </div>
         <?php if ($comment->comment_approved == '0') : ?>
         <p><em><strong>Please Note:</strong> Your comment is awaiting moderation.</em></p>
         <?php endif; ?>
