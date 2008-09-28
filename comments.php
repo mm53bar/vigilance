f3860795cc42007d09a11afcb361efe8d431b9f5
Drew Strojny <dstrojny@jestro.com>
2008-09-28 17:17:19 -0600

    cleaned up comments.php code

--- comments.php
+++ comments.php
@@ -24,15 +24,14 @@
 		<div class="c-single clear <?php if($isByAuthor ) { echo 'admin';} ?> <?php echo $oddcomment; ?>" id="comment-<?php comment_ID() ?>" >
 			<div class="c-grav"><?php echo get_avatar( get_comment_author_email(), '80' )?></div>
 			<div class="c-body">
-      <div class="c-date"><span><?php comment_date('Y'); ?></span> <?php comment_date('F j'); ?></div>
+        <div class="c-date">
+          <span><?php comment_date('Y'); ?></span> <?php comment_date('F j'); ?>
+        </div>
         <div class="c-head">
-          <?php comment_author_link() ?>
-          <span class="c-permalink">
-            <a href="<?php echo get_permalink(); ?>#comment-<?php comment_ID(); ?>"> permalink</a>
-          </span>
+          <?php comment_author_link() ?><span class="c-permalink"><a href="<?php echo get_permalink(); ?>#comment-<?php comment_ID(); ?>"> permalink</a></span>
         </div>
         <?php if ($comment->comment_approved == '0') : ?>
-        <p><em><strong>Please Note:</strong> Your comment is awaiting moderation.</em></p>
+          <p><em><strong>Please Note:</strong> Your comment is awaiting moderation.</em></p>
         <?php endif; ?>
 				<?php comment_text() ?>
         <?php comment_type((''),('Trackback'),('Pingback')); ?>
