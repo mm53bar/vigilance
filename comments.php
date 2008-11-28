b0e002d9345e530ea62fa4b994c46232a3080571
Drew Strojny <dstrojny@jestro.com>
2008-11-27 23:20:44 -0700

    fix legacy comment filter bug that is causing fatal errors

--- comments.php
+++ comments.php
@@ -1,12 +1,4 @@
 <?php
-add_filter('comments_template', 'legacy_comments');
-function legacy_comments($file) {
-	if(!function_exists('wp_list_comments')) 	$file = TEMPLATEPATH . '/legacy.comments.php';
-	return $file;
-}
-?>
-  
-<?php
 // Do not delete these lines
 	if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
 		die ('Please do not load this page directly. Thanks!');
