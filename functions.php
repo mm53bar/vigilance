c81ae59f30d8f99b150ad0ff4b98d802da53f645
Drew Strojny <dstrojny@jestro.com>
2008-11-28 10:17:38 -0700

    fix formatting in functions.php

--- functions.php
+++ functions.php
@@ -4,9 +4,8 @@ define ('COPY', FUNCTIONS . '/vigilance.php');
 require_once (FUNCTIONS . '/sidebars.php');
 require_once (FUNCTIONS . '/comments.php');
 require_once (FUNCTIONS . '/vigilance-admin.php');
-?>
 
-<?php /*---Use legacy comments template for versions less than 2.7---*/
+// Use legacy comments template for versions less than 2.7
 add_filter('comments_template', 'legacy_comments');
 function legacy_comments($file) {
 	if(!function_exists('wp_list_comments')) 	$file = TEMPLATEPATH . '/legacy.comments.php';
