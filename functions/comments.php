9fb12dd31ad6e7e237a2d1d9dbf2154b6ae1679c
unknown <dstrojny@jestro.com>
2008-12-24 08:57:56 -0700

    remove empty line

--- functions/comments.php
+++ functions/comments.php
@@ -19,8 +19,7 @@ function comment_count( $count ) {
             <span><?php comment_date('Y'); ?></span> <?php comment_date('F j'); ?>
           </div>
           <div class="c-head">
-            <?php comment_author_link() ?> <span class="c-permalink"><a href="<?php echo get_permalink(); ?>#comment-<?php comment_ID(); ?>">permalink</a></span>
-            
+            <?php comment_author_link() ?> <span class="c-permalink"><a href="<?php echo get_permalink(); ?>#comment-<?php comment_ID(); ?>">permalink</a></span>   
           </div>
           <?php if ($comment->comment_approved == '0') : ?>
             <p><em><strong>Please Note:</strong> Your comment is awaiting moderation.</em></p>
