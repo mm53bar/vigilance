ccbd476f1dbde42e591574a77a8bc68eb4a0c02d
dstrojny <dstrojny@jestro.com>
2009-08-28 15:30:45 -0600

    duplicate admin template to prep for overhaul

---
+++ functions/vigilance-admin-old.php
@@ -0,0 +1,181 @@
+<?php 
+function mytheme_add_admin() {
+	global $themename, $shortname, $options;
+	if ( $_GET['page'] == basename(__FILE__) ) {
+  	if ( 'save' == $_REQUEST['action'] ) {
+			foreach ($options as $value) {
+      	update_option( $value['id'], $_REQUEST[ $value['id'] ] ); 
+			}
+			foreach ($options as $value) {
+				
+        if ( isset( $_REQUEST[ $value['id'] ] ) ) { 
+					update_option( $value['id'], $_REQUEST[ $value['id'] ]  ); 
+				} 
+				else { 
+					delete_option( $value['id'] ); 
+				} 
+			}		
+			header("Location: themes.php?page=vigilance-admin.php&saved=true");
+	   	die;
+		} 
+		else if( 'reset' == $_REQUEST['action'] ) {
+			foreach ($options as $value) {
+      	delete_option( $value['id'] ); 
+			}
+		header("Location: themes.php?page=vigilance-admin.php&reset=true");
+    die;
+		}
+  }
+	add_theme_page($themename." Options", "$themename Options", 'edit_themes', basename(__FILE__), 'mytheme_admin');
+}
+
+function headimage_admin(){
+	
+}
+
+function mytheme_admin() {
+
+    global $themename, $shortname, $options;
+
+    if ( $_REQUEST['saved'] ) echo '<div id="message" class="updated fade"><p><strong>'.$themename.' settings saved.</strong></p></div>';
+    if ( $_REQUEST['reset'] ) echo '<div id="message" class="updated fade"><p><strong>'.$themename.' settings reset.</strong></p></div>';
+    
+?>
+<div class="wrap">
+<h2 class="updatehook" style=" padding-top: 20px; font-size: 2.8em;"><?php printf(__ ('%s Options', 'vigilance'), $themename)?></h2>
+<p style="line-height: 1.6em; font-size: 1.2em; width: 75%;"><?php _e('Welcome to the Vigilance Options menu. If you have any questions or need support please sign up for a <a href="https://themes.jestro.com/members/signup.php">PRO Membership</a>. Vigilance was hand coded and brought to you by <a href="http://www.jestro.com/">Jestro</a>.', 'vigilance'); ?></p>
+<form method="post">
+
+<table class="form-table">
+
+<?php foreach ($options as $value) { 
+	
+	switch ( $value['type'] ) {
+		case 'subhead':
+		?>
+			</table>
+			
+			<h3 style="padding-top: 15px;"><?php echo $value['name']; ?></h3>
+			
+			<table class="form-table">
+		<?php
+		break;
+		case 'text':
+		option_wrapper_header($value);
+		?>
+		        <input name="<?php echo $value['id']; ?>" id="<?php echo $value['id']; ?>" type="<?php echo $value['type']; ?>" value="<?php if ( get_settings( $value['id'] ) != "") { echo get_settings( $value['id'] ); } else { echo $value['std']; } ?>" />
+		<?php
+		option_wrapper_footer_nobreak($value);
+		break;
+		
+		case 'select':
+		option_wrapper_header($value);
+		?>
+	            <select name="<?php echo $value['id']; ?>" id="<?php echo $value['id']; ?>">
+	                <?php foreach ($value['options'] as $key=>$option) { ?>
+	                <option <?php if ( get_settings( $value['id'] ) == $key) { echo ' selected="selected"'; } elseif ($option == $value['std']) { echo ' selected="selected"'; } ?> value="<?php echo $key; ?>"><?php echo $option; ?></option>
+	                <?php } ?>
+	            </select>
+		<?php
+		option_wrapper_footer($value);
+		break;
+		
+		case 'textarea':
+		$ta_options = $value['options'];
+		option_wrapper_header($value);
+		?>
+				<textarea name="<?php echo $value['id']; ?>" id="<?php echo $value['id']; ?>" cols="<?php echo $ta_options['cols']; ?>" rows="<?php echo $ta_options['rows']; ?>"><?php 
+				if( get_settings($value['id']) != "") {
+						echo stripslashes(get_settings($value['id']));
+					}else{
+						echo stripslashes($value['std']);
+				}?></textarea>
+		<?php
+		option_wrapper_footer($value);
+		break;
+
+		case "radio":
+		option_wrapper_header($value);
+		
+ 		foreach ($value['options'] as $key=>$option) { 
+				$radio_setting = get_settings($value['id']);
+				if($radio_setting != ''){
+		    		if ($key == get_settings($value['id']) ) {
+						$checked = "checked=\"checked\"";
+						} else {
+							$checked = "";
+						}
+				}else{
+					if($key == $value['std']){
+						$checked = "checked=\"checked\"";
+					}else{
+						$checked = "";
+					}
+				}?>
+	            <input type="radio" name="<?php echo $value['id']; ?>" value="<?php echo $key; ?>" <?php echo $checked; ?> /><?php echo $option; ?><br />
+		<?php 
+		}
+		 
+		option_wrapper_footer_nobreak($value);
+		break;
+		
+		case "checkbox":
+		option_wrapper_header($value);
+						if(get_settings($value['id'])){
+							$checked = "checked=\"checked\"";
+						}else{
+							$checked = "";
+						}
+					?>
+		            <input type="checkbox" name="<?php echo $value['id']; ?>" id="<?php echo $value['id']; ?>" value="true" <?php echo $checked; ?> />
+		<?php
+		option_wrapper_footer_nobreak($value);
+		break;
+
+		default:
+
+		break;
+	}
+}
+?>
+
+</table>
+
+<p class="submit">
+<input name="save" type="submit" value="Save changes" />    
+<input type="hidden" name="action" value="save" />
+</p>
+</form>
+<form method="post">
+<p class="submit">
+<input name="reset" type="submit" value="Reset" />
+<input type="hidden" name="action" value="reset" />
+</p>
+</form>
+<?php
+}
+
+function option_wrapper_header($values){
+	?>
+	<tr valign="top"> 
+	    <th scope="row"><?php echo $values['name']; ?>:</th>
+	    <td>
+	<?php
+}
+function option_wrapper_footer($values){
+	?>
+		<br />
+		<?php echo $values['desc']; ?>
+	    </td>
+	</tr>
+	<?php 
+}
+function option_wrapper_footer_nobreak ($values){
+	?>
+		<?php echo $values['desc']; ?>
+	    </td>
+	</tr>
+	<?php 
+}
+add_action('admin_menu', 'mytheme_add_admin'); 
+?>
