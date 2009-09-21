71a6fe1a6c33bb9357e82759b9475dd9d79aea9c
Michael McClenaghan <mike@sideline.ca>
2009-09-21 08:00:32 -0600

    Added thumbnail generation and reworked front page to highlight featured articles

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php get_header(); ?>
+<div id="headline">
+<?php query_posts("showposts=1&category_name=Featured"); ?>
 		<?php if (have_posts()) : ?>
 		<?php while (have_posts()) : the_post(); ?>
 		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -12,6 +14,10 @@
 				<div class="author"><?php printf(__ ('by %s', 'vigilance'), get_the_author())?></div>
 			</div><!--end meta-->
 			<div class="entry clear">
+<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+<img src="<?php echo bloginfo('template_url'); ?>/scripts/timthumb.php?src=/<?php
+echo get_first_image(); ?>&w=300&h=275&zc=1&q=100"
+alt="<?php the_title_attribute(); ?>" class="left" width="300px" height="275px"  /></a>			
 				<?php the_excerpt(); ?>
         <?php edit_post_link(__('Edit This','<p>','</p>', 'vigilance')); ?>
 				<?php wp_link_pages(); ?>
@@ -20,13 +26,61 @@
 				<p><?php _e('from &rarr;', 'vigilance') ?> <?php the_category(__(', ', 'vigilance')); ?></p>
 			</div><!--end post footer-->
 		</div><!--end post-->
-		<?php endwhile; /* rewind or continue if all posts have been fetched */ ?>
-		<div class="navigation index">
-			<div class="alignleft"><?php next_posts_link(__ ('&laquo; Older Entries', 'vigilance')) ?></div>
-			<div class="alignright"><?php previous_posts_link(__ ('Newer Entries &raquo;', 'vigilance')) ?></div>
-		</div><!--end navigation-->
-		<?php else : ?>
+		<?php endwhile; ?>
 		<?php endif; ?>
+</div>
+
+	<div id="featured">
+	<?php query_posts("showposts=3&offset=1&category_name=Featured"); ?>
+			<?php if (have_posts()) : ?>
+			<?php while (have_posts()) : the_post(); ?>
+			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<div class="post-header">
+					<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+					<div class="date"><span><?php the_time('Y') ?></span> <?php the_time(__ ('F j', 'vigilance')) ?></div>
+					<div class="comments"><?php comments_popup_link(__ ('Leave a comment', 'vigilance'), __ ('1 Comment', 'vigilance'), __ngettext ('% Comment', '% Comments', get_comments_number (),'vigilance')); ?></div>
+				</div><!--end post header-->
+				<div class="meta clear">
+					<div class="tags"><?php the_tags(__('tags: ', ', ', '', 'vigilance')); ?></div>
+					<div class="author"><?php printf(__ ('by %s', 'vigilance'), get_the_author())?></div>
+				</div><!--end meta-->
+				<div class="entry clear">
+	<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+	<img src="<?php echo bloginfo('template_url'); ?>/scripts/timthumb.php?src=/<?php
+	echo get_first_image(); ?>&w=100&h=65&zc=1&q=100"
+	alt="<?php the_title_attribute(); ?>" class="left" width="100px" height="65px"  /></a>			
+					<?php the_excerpt(); ?>
+	        <?php edit_post_link(__('Edit This','<p>','</p>', 'vigilance')); ?>
+					<?php wp_link_pages(); ?>
+				</div><!--end entry-->
+				<div class="post-footer">
+					<p><?php _e('from &rarr;', 'vigilance') ?> <?php the_category(__(', ', 'vigilance')); ?></p>
+				</div><!--end post footer-->
+			</div><!--end post-->
+			<?php endwhile; ?>
+			<?php endif; ?>
+	</div>
+
+
+		<div id="asides">
+		<?php query_posts("showposts=5&category_name=Asides"); ?>
+				<?php if (have_posts()) : ?>
+				<?php while (have_posts()) : the_post(); ?>
+				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<div class="post-header">
+						<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+						<div class="date"><span><?php the_time('Y') ?></span> <?php the_time(__ ('F j', 'vigilance')) ?></div>
+						<div class="comments"><?php comments_popup_link(__ ('Leave a comment', 'vigilance'), __ ('1 Comment', 'vigilance'), __ngettext ('% Comment', '% Comments', get_comments_number (),'vigilance')); ?></div>
+					</div><!--end post header-->
+						<div class="post-footer">
+							<p><?php _e('from &rarr;', 'vigilance') ?> <?php the_category(__(', ', 'vigilance')); ?></p>
+						</div><!--end post footer-->
+				</div><!--end post-->
+				<?php endwhile; ?>
+				<?php endif; ?>
+		</div>
+		
+		
 	</div><!--end content-->
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
